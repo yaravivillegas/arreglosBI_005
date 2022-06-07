410c8f438f2ad59043966757fee65264c1b7273a
yaravi <yaravi.villegas@gmail.com>
2022-06-07 12:04:25 -0400

    termino del ejercicio

--- ejercicio1Bi.py
+++ ejercicio1Bi.py
@@ -4,7 +4,7 @@
 '''
 import numpy as np
 import random
-print("Bienbenidos a la bidimensión")
+
 matriz = np.diag([1,1,1])
 print(matriz)
 
@@ -13,4 +13,39 @@ for i in range(3):
         matriz[i][j] = random.randint(0,100)
         
 print(matriz)
-print("Chao pescao")
+
+#Promedio de los elementos.
+suma = matriz.sum()
+promedio = suma / 9
+
+print(f"suma: {suma}")
+print(f"promedio: {promedio}")
+
+#Suma de los elementos.
+acumulador = 0
+for i in range(3):
+    for j in range(3):
+        acumulador = acumulador + matriz[i][j] 
+print(f"suma: {acumulador}")
+
+#Mostrar el elemento mayor y menor.
+mayor = 0
+menor = 100
+for i in range(3):
+    for j in range(3):
+        if matriz[i][j] > mayor:
+            mayor = matriz[i][j]
+        if matriz[i][j] < menor:
+            menor = matriz[i][j]
+            
+print(f"mayor: {mayor}")
+print(f"menor: {menor}")
+
+#Mostrar sólo los elementos de la diagonal principal.
+print("diagonal\n")
+for i in range(3):
+    for j in range(3):
+        if i == j:
+            print(matriz[i][j])
+print("\n...............")
+            
